a13cf9f5167271ff358b59a1a2f203fa0fb21750
horsicq <horsicq@gmail.com>
2024-02-28 19:06:15 +0100

    Update file(s): 2024-02-28

--- xvisualizationwidget.cmake
+++ xvisualizationwidget.cmake
@@ -1,11 +1,17 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 set(XVISUALIZATIONWIDGET_SOURCES
+    ${CMAKE_CURRENT_LIST_DIR}/dialogvisualizationprocess.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogvisualizationprocess.h
     ${CMAKE_CURRENT_LIST_DIR}/xfilearrow.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xfilearrow.h
     ${CMAKE_CURRENT_LIST_DIR}/xfiledescription.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xfiledescription.h
     ${CMAKE_CURRENT_LIST_DIR}/xfileimage.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xfileimage.h
     ${CMAKE_CURRENT_LIST_DIR}/xvisualization.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xvisualization.h
+    ${CMAKE_CURRENT_LIST_DIR}/xvisualizationwidget.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xvisualizationwidget.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xvisualizationwidget.ui
-    ${CMAKE_CURRENT_LIST_DIR}/dialogvisualizationprocess.cpp
     )
